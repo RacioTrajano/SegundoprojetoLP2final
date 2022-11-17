afa9a49966043b5ed8c3409f1b81aea41ba76d01
RacioTrajano <85686582+RacioTrajano@users.noreply.github.com>
2022-11-17 09:41:49 -0300

    Add files via upload

---
+++ projetolp2atual1-main/projetolp2atual1-main/ProjetoUnd2-main/ProjetoUnd2/src/br/ufrn/imd/dominio/Revista.java
@@ -0,0 +1,25 @@
+package br.ufrn.imd.dominio;
+
+public class Revista extends ItemAcervo{
+
+	public Revista() {
+		
+	}
+	
+	private String tema;
+
+	public String getTema() {
+		return tema;
+	}
+
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "Revista(s): \n" +"Título: " + titulo + " Tema: " + tema;
+	}
+	
+}
